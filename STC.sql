ad5e0d13f4ea53df9e393d106bc12e4d3e2049cb
Hassan Khalaf <62292037+7assan5alaf@users.noreply.github.com>
2023-12-13 19:04:22 +0200

    Update STC.sql

--- STC.sql
+++ STC.sql
@@ -1,7 +1,6 @@
-use User_Training
-Select u.user_id,p.username, u.training_date,COUNT(u.training_id)as 'count'
-from user_training u
-join person p on u.user_id=p.user_id
-Group By u.user_id,u.training_id,u.training_date,p.username
-having  COUNT(u.training_id)>1
-order by training_date desc 
+SELECT u.user_id,p.username, u.training_date,COUNT(u.training_id)AS 'count'
+FROM user_training u
+JOIN person p ON u.user_id=p.user_id
+GROUP BY u.user_id,u.training_id,u.training_date,p.username
+HAVING  COUNT(u.training_id)>1
+ORDER BY training_date DESC 
