2eab6331b275756156b802aa0d96ccaba5871989
Hassan Khalaf <62292037+7assan5alaf@users.noreply.github.com>
2023-12-19 12:31:04 +0200

    Update ItemResponse.java

--- spring-boot-file-upload/src/main/java/com/stc/filemngt/view/ItemResponse.java
+++ spring-boot-file-upload/src/main/java/com/stc/filemngt/view/ItemResponse.java
@@ -3,14 +3,13 @@ package com.stc.filemngt.view;
 public class ItemResponse {
 
 	private byte []file;
-	private String file_name;
+	private String file_path;
 	private String group_name;
 	
-	
-	public ItemResponse(byte[] file, String file_name, String group_name) {
+	public ItemResponse(byte[] file, String file_path, String group_name) {
 		super();
 		this.file = file;
-		this.file_name = file_name;
+		this.file_path = file_path;
 		this.group_name = group_name;
 	}
 	public byte[] getFile() {
@@ -19,11 +18,11 @@ public class ItemResponse {
 	public void setFile(byte[] file) {
 		this.file = file;
 	}
-	public String getFile_name() {
-		return file_name;
+	public String getFile_path() {
+		return file_path;
 	}
-	public void setFile_name(String file_name) {
-		this.file_name = file_name;
+	public void setFile_path(String file_path) {
+		this.file_path = file_path;
 	}
 	public String getGroup_name() {
 		return group_name;
