a1008544009105e70ef44a7a0b0b92240578de47
Hassan Khalaf <62292037+7assan5alaf@users.noreply.github.com>
2024-01-21 13:42:37 +0200

    Delete upload-file directory

--- upload-file/src/main/java/com/stc/filemngt/error/RecordNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.stc.filemngt.error;
-
-public class RecordNotFoundException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-	public RecordNotFoundException(String msg) {
-		super(msg);
-	}
-	
-}
